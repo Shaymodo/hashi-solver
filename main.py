9e2b9ba4176cb0028f8cb42dc3af89a6cbb4bb6a
CJ Howell <ninjacal91@gmail.com>
2025-01-17 11:12:57 -0700

    Implemented PuzzleDigit class structure, expanded solver function

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from multiprocessing import Value
 import sys
 
+
+class PuzzleDigit:                              # Class for each puzzle number that isn't 0
+    def __init__(self, num):
+        self.full_num = num                     # Initialize the actual number
+        self.remaining_num = num                # Initialize the number counting down for each line drawn
+
+    def done(self):                             # Returns True if all the lines drawn for this number
+        return self.remaining_num == 0
+
+    def number(self):                           # Returns the int value of the full_num
+        return self.full_num
+
+    def add_horizontal(self):
+        pass
+
+    def add_vertical(self):
+        pass
+
+
 def main():
     layout = []
     height, width = 0, 0
@@ -13,48 +32,49 @@ def main():
         print("4: Solve current puzzle")
         print("5: Quit")
         choice = int(input("Please select an option: "))
-        print("\033[H\033[J")  # Clears the screen and moves the cursor to the top-left corner
+        print("\033[H\033[J", end="")       # Clears the screen
 
 
-        if choice == 1:                 # Manual Entry
+        if choice == 1:                     # Manual Entry
             try:
                 while height < 4: height = int(input("Please enter the height of the puzzle: "))
                 while width < 4: width = int(input("Please enter the width of the puzzle: "))
             except ValueError:
                 print("Input a valid integer")
-                print()
                 break
             layout, num_total = manual_layout_input(width, height)
+            print("\033[H\033[J", end="")   # Clears the screen
 
-        elif choice == 2:               # Automatic Entry
+        elif choice == 2:                   # Automatic Entry
             print("This isn't implemented yet, I just told you that")
-            print()
 
-        elif choice == 3:               # View current puzzle
+        elif choice == 3:                   # View current puzzle
             if layout != []:
                 for i in range(height):
-                    print(layout[i])
-                    print()
+                    print("[", end=" ")
+                    for j in range(width):
+                        print (f"{layout[i][j].number()}", end=" ")
+                    print("]")
             else:
                 print("Current puzzle is not valid")
-                print()
 
-        elif choice == 4:               # Solve current puzzle
+        elif choice == 4:                   # Solve current puzzle
             if layout != []:
                 solver(layout, num_total)
-            else:
-                print("Current puzzle is not valid")
-                print()
+            else:                           # If layout is empty, puzzle does not exist
+                print("No current puzzle")
 
-        elif choice == 5:               # Quit
+        elif choice == 5:                   # Quit
             sys.exit()
 
-        else:                           # Error message for invalid input
+        else:                               # Error message for invalid input
             print("Not a valid choice")
+
+        print()
 
 
 def manual_layout_input(width, height):     # Iterates through the 2D array and asks for user input for each value
-    layout = []           # Creates empty row and column arrays
+    layout = []                             # Creates empty row and column arrays
     num_total = 0
     for i in range (height):
         layout_inner = []
@@ -62,15 +82,19 @@ def manual_layout_input(width, height):     # Iterates through the 2D array and 
         print (f"Row {i+1}")
         for j in range (width):
             while True:
-                try:                        # Ensure input is an int
-                    val = int(input("Please input the next number 0-8, enter 0 if empty: "))
-                    if val < 0 or val > 8:
+                try:                            # Ensure input is an int
+                    num = int(input("Please input the next number 0-8, enter 0 if empty: "))
+                    if num < 0 or num > 8:
                         print("Not between 0 and 8, try again")
                     else:
-                        layout_inner.append(val)        # Adds valid value to the row
-                        print(layout_inner)             # Displays row for user to see
-                        if val != 0:
+                        valid_digit = PuzzleDigit(num)
+                        layout_inner.append(valid_digit)        # Adds object with valid digit to the row
+                        print("[", end=" ")
+                        for k in range (len(layout_inner)):     # Displays row for user to see
+                            print (f"{layout_inner[k].number()}", end=" ")
+                        if num != 0:
                             num_total += 1
+                        print("]")
                         break
                 except ValueError:
                     print("Your input was not an integer, try again")
@@ -79,37 +103,40 @@ def manual_layout_input(width, height):     # Iterates through the 2D array and 
                     
     return layout, num_total
 
-def solver(layout, num_total):                         # Calls solver helper functions depending on the value of each number in the layout
+def solver(layout, num_total):                      # Calls solver helper functions depending on the value of each number in the layout
     solved = False
     lined_layout = layout
     while not solved:                               # Loops until puzzle is solved
         finished_nums = 0
         for i in range(len(layout)):
             for j in range(len(layout[i])):
-                num = layout[i][j]
-                if num == 1:
-                    lined_layout,done = solver_one(lined_layout)
-                elif num == 2:
-                    lined_layout,done = solver_two(lined_layout)                # MAKE NUMS OBJECTS OF A CLASS!!!
-                elif num == 3:
-                    lined_layout,done = solver_three(lined_layout)
-                elif num == 4:
-                    lined_layout,done = solver_four(lined_layout)
-                elif num == 5:
-                    lined_layout,done = solver_five(lined_layout)
-                elif num == 6:
-                    lined_layout,done = solver_six(lined_layout)
-                elif num == 7:
-                    lined_layout,done = solver_seven(lined_layout)
-                elif num == 8:
-                    lined_layout,done = solver_eight(lined_layout)
+                if (layout[i][j].done()):                  # Checks object to see if all lines are drawn
+                    finished_nums += 1
+                else:
+                    print(layout[i][j].done())     #TEMP
+                    num = layout[i][j].number()
+                    if num == 1:
+                        lined_layout = solver_one(lined_layout)
+                    elif num == 2:
+                        lined_layout = solver_two(lined_layout)
+                    elif num == 3:
+                        lined_layout = solver_three(lined_layout)
+                    elif num == 4:
+                        lined_layout = solver_four(lined_layout)
+                    elif num == 5:
+                        lined_layout = solver_five(lined_layout)
+                    elif num == 6:
+                        lined_layout = solver_six(lined_layout)
+                    elif num == 7:
+                        lined_layout = solver_seven(lined_layout)
+                    elif num == 8:
+                        lined_layout = solver_eight(lined_layout)
 
-                if num == 
+        if finished_nums == num_total:
+            solved = True                               # Ends the loop
 
-
-        solved = True                               # Ends the loop  
-
-    pass
+        solved = True                           # TEMPORARY TO PREVENT INFINITE LOOPING
+    return
 
 def solver_one(layout):
     pass
