991fe415f59924479341fe81d2627f10da652f8b
CJ Howell <ninjacal91@gmail.com>
2025-01-17 00:13:01 -0700

    continued structure building of solver functiosn

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
                 print("Input a valid integer")
                 print()
                 break
-            layout = manual_layout_input(width, height)
+            layout, num_total = manual_layout_input(width, height)
 
         elif choice == 2:               # Automatic Entry
             print("This isn't implemented yet, I just told you that")
@@ -41,7 +41,7 @@ def main():
 
         elif choice == 4:               # Solve current puzzle
             if layout != []:
-                solver(layout)
+                solver(layout, num_total)
             else:
                 print("Current puzzle is not valid")
                 print()
@@ -55,6 +55,7 @@ def main():
 
 def manual_layout_input(width, height):     # Iterates through the 2D array and asks for user input for each value
     layout = []           # Creates empty row and column arrays
+    num_total = 0
     for i in range (height):
         layout_inner = []
         print()
@@ -68,20 +69,44 @@ def manual_layout_input(width, height):     # Iterates through the 2D array and 
                     else:
                         layout_inner.append(val)        # Adds valid value to the row
                         print(layout_inner)             # Displays row for user to see
+                        if val != 0:
+                            num_total += 1
                         break
                 except ValueError:
                     print("Your input was not an integer, try again")
 
         layout.append(layout_inner)                 # Adds each row here
                     
-    return layout
+    return layout, num_total
 
-def solver(layout):                         # Calls solver helper functions depending on the value of each number in the layout
+def solver(layout, num_total):                         # Calls solver helper functions depending on the value of each number in the layout
     solved = False
+    lined_layout = layout
     while not solved:                               # Loops until puzzle is solved
+        finished_nums = 0
         for i in range(len(layout)):
             for j in range(len(layout[i])):
                 num = layout[i][j]
+                if num == 1:
+                    lined_layout,done = solver_one(lined_layout)
+                elif num == 2:
+                    lined_layout,done = solver_two(lined_layout)                # MAKE NUMS OBJECTS OF A CLASS!!!
+                elif num == 3:
+                    lined_layout,done = solver_three(lined_layout)
+                elif num == 4:
+                    lined_layout,done = solver_four(lined_layout)
+                elif num == 5:
+                    lined_layout,done = solver_five(lined_layout)
+                elif num == 6:
+                    lined_layout,done = solver_six(lined_layout)
+                elif num == 7:
+                    lined_layout,done = solver_seven(lined_layout)
+                elif num == 8:
+                    lined_layout,done = solver_eight(lined_layout)
+
+                if num == 
+
+
         solved = True                               # Ends the loop  
 
     pass
